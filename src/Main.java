e406805500d835899f2d8a19d5f03425b2bec947
kngssd <danakengessova@gmail.com>
2024-08-13 15:49:35 +0100

    added interface Agent to continue polymorphism with FastAgent and SlowAgent

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import processing.core.PApplet;
 
 public class Main extends PApplet {
     Agent[] agents;
+
     public static void main(String[] args) {
         PApplet.main(new String[]{"Main"});
     }
+
     @Override
     public void settings() {
         size(800, 600);
@@ -12,19 +14,31 @@ public class Main extends PApplet {
 
     @Override
     public void setup() {
-        agents = createAgents(this, 100);
+        agents = createAgents(this);
     }
 
 
     @Override
     public void draw() {
-    background(100);
-        for (Agent a : agents){
+        background(100);
+        for (Agent a : agents) {
             a.display();
         }
 
-        for (Agent a : agents){
+        for (Agent a : agents) {
             a.update();
         }
+    }
+
+    private Agent[] createAgents(PApplet p5) {
+        agents = new Agent[100];
+        for (int i = 0; i < 100; i++) {
+            if (p5.random(1) > 0.5) {
+                agents[i] = createFastAgent(p5);
+            } else {
+                agents[i] = createSlowAgent(p5);
+            }
+        }
+        return agents;
     }
 }
